18ed23185b4962204139b199fceedda837f945bb
RAHUL CHAUHAN <63314244+Rahul-Chauhan-2212@users.noreply.github.com>
2023-04-11 20:27:10 +0530

    Update schema_creation.sql

--- ShoppingCart/schema_creation.sql
+++ ShoppingCart/schema_creation.sql
@@ -1,9 +1,12 @@
 -- Run this script as SYSTEM user to create or reset the schema from scratch.
---to alter oracle session---
-alter session set "_ORACLE_SCRIPT"=true;  
 
----creation of user----
 drop user shopping_cart cascade; -- drop existing user
 
 create user shopping_cart identified by welcome DEFAULT TABLESPACE users TEMPORARY TABLESPACE temp; -- create user
 grant all privileges to shopping_cart; -- grant privileges
+
+---for postgres,
+drop user shopping_cart; -- drop existing user
+create user shopping_cart with password 'welcome'; -- create user
+drop database shopping_cart; -- drop existing database
+create database shopping_cart; -- create database
